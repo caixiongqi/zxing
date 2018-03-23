edd37b6a44ce5f17a1ab053497f1bcea670cb8f2
caixiongqi <caixiongqi@sinosoft.com.cn>
2018-03-23 17:18:50 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+# 二维码生成DEMO
+
+采用google的zxing包生成二维码。
+
+```xml
+<dependency>
+  <groupId>com.google.zxing</groupId>
+  <artifactId>core</artifactId>
+  <version>3.3.0</version>
+</dependency>
+```
+
---
+++ src/main/java/com/zxing/zxing_make/ZxingMakeApplication.java
@@ -0,0 +1,12 @@
+package com.zxing.zxing_make;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZxingMakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZxingMakeApplication.class, args);
+	}
+}
